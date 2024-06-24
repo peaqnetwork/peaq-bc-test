1c7144216f8eeeee03722cd75f3ff6ec9ad37d2c
jaypan <pan@peaq.io>
2024-06-24 17:36:58 +0200

    Add checkings

--- tests/utils_func.py
+++ tests/utils_func.py
@@ -4,6 +4,7 @@ from tools.utils import WS_URL
 from peaq.utils import get_chain
 from tools.restart import restart_parachain_launch
 from tools.runtime_upgrade import do_runtime_upgrade
+from tools.runtime_upgrade import wait_until_block_height
 from substrateinterface import SubstrateInterface
 
 
@@ -15,11 +16,18 @@ def get_runtime_upgrade_path():
     return os.environ.get('RUNTIME_UPGRADE_PATH')
 
 
+# Will raise error
 def restart_parachain_and_runtime_upgrade():
     restart_parachain_launch()
+    wait_until_block_height(SubstrateInterface(url=WS_URL), 1)
+    substrate = SubstrateInterface(url=WS_URL)
+    old_version = substrate.get_block_runtime_version(substrate.get_block_hash())['specVersion']
     if is_runtime_upgrade_test():
         path = get_runtime_upgrade_path()
         do_runtime_upgrade(path)
+        new_version = substrate.get_block_runtime_version(substrate.get_block_hash())['specVersion']
+        if old_version == new_version:
+            raise Exception(f'Runtime upgrade failed. old_version: {old_version}, new_version: {new_version}')
 
 
 def is_not_dev_chain():
