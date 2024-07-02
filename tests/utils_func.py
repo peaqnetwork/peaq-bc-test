7a9034c5e89128dc4d472dbf2c3be64425ca0e0a
jaypan <pan@peaq.io>
2024-07-02 22:25:55 +0200

    Update the inflation improvement test

--- tests/utils_func.py
+++ tests/utils_func.py
@@ -3,6 +3,7 @@ import os
 from tools.utils import WS_URL
 from peaq.utils import get_chain
 from tools.restart import restart_parachain_launch
+from tools.utils import get_modified_chain_spec
 from tools.runtime_upgrade import do_runtime_upgrade
 from tools.runtime_upgrade import wait_until_block_height
 from substrateinterface import SubstrateInterface
@@ -35,6 +36,12 @@ def is_not_dev_chain():
     chain_name = get_chain(ws)
     print(f'chain_name: {chain_name}')
     return chain_name not in ['peaq-dev', 'peaq-dev-fork']
+
+
+def is_krest_related_chain():
+    ws = SubstrateInterface(url=WS_URL)
+    chain_name = get_chain(ws)
+    return chain_name in ['krest-network', 'krest-network-fork']
 
 
 def is_local_new_chain():
