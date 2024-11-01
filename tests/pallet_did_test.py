84231bdc6c5ecacfe040062bbdf8e36bb4655261
jaypan <pan@eotlabs.io>
2024-11-01 15:53:30 +0100

    Add deposit testing

--- tests/pallet_did_test.py
+++ tests/pallet_did_test.py
@@ -7,6 +7,17 @@ from tools.utils import get_balance_reserve_value
 from tools.constants import WS_URL
 from peaq.utils import ExtrinsicBatch
 from peaq.did import did_add_payload, did_update_payload, did_remove_payload, did_rpc_read
+from tools.runtime_upgrade import wait_until_block_height
+from tools.constants import PARACHAIN_WS_URL, RELAYCHAIN_WS_URL
+from peaq.utils import get_chain
+from tools.utils import get_modified_chain_spec
+
+
+DID_MIN_DEPOSIT = {
+    'peaq-dev': 0.1 * 10 ** 18,
+    'krest-network': 0.1 * 10 ** 18,
+    'peaq-network': 0.005 * 10 ** 18,
+}
 
 
 @pytest.mark.substrate
@@ -14,6 +25,10 @@ class TestPalletDid(unittest.TestCase):
     def setUp(self):
         self.substrate = SubstrateInterface(url=WS_URL)
         self.kp_src = Keypair.create_from_uri('//Alice')
+        wait_until_block_height(SubstrateInterface(url=RELAYCHAIN_WS_URL), 1)
+        wait_until_block_height(SubstrateInterface(url=PARACHAIN_WS_URL), 1)
+        self.chain_spec = get_chain(self.substrate)
+        self.chain_spec = get_modified_chain_spec(self.chain_spec)
 
     def test_did_add(self):
         reserved_before = get_balance_reserve_value(self.substrate, self.kp_src.ss58_address, 'peaq_did')
@@ -32,6 +47,7 @@ class TestPalletDid(unittest.TestCase):
         self.assertEqual(data['value'], value)
         reserved_after = get_balance_reserve_value(self.substrate, self.kp_src.ss58_address, 'peaq_did')
         self.assertGreater(reserved_after, reserved_before)
+        self.assertGreaterEqual(reserved_after - reserved_before, DID_MIN_DEPOSIT[self.chain_spec])
 
     def test_did_update(self):
         name = int(time.time())
