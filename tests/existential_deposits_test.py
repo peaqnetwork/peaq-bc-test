b6190deb820dc56caa83dfdc06da08592a12ecf6
jaypan <pan@eotlabs.io>
2024-11-10 09:15:41 +0000

    Fix error
    test on peaq-network-node 1208684949341989_merge_back

--- tests/existential_deposits_test.py
+++ tests/existential_deposits_test.py
@@ -3,7 +3,6 @@ import unittest
 from substrateinterface import SubstrateInterface, Keypair
 from tools.utils import get_existential_deposit
 from tools.constants import WS_URL
-from peaq.extrinsic import transfer
 
 
 @pytest.mark.substrate
@@ -19,17 +18,4 @@ class TestExitentialDeposits(unittest.TestCase):
 
     def test_local_token(self):
         token = self.get_existential_deposit()
-        self.assertGreater(token, 2)
-        token /= 2
-
-        # Execute -> Send local token to another account but below the Exitential Deposits
-        receipt = transfer(
-            self.substrate,
-            self.alice,
-            self.kp.ss58_address,
-            token,
-            1
-        )
-
-        # Check: the error happens
-        self.assertFalse(receipt.is_success)
+        self.assertEqual(token, 0)
