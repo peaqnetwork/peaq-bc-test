f3d724039af034a15edb0b49c514735ea5e76778
jaypan <pan@eotlabs.io>
2025-07-23 09:44:27 +0200

    Add the testing for the bridge parachain staking module

--- tests/bridge_parachain_staking_test.py
+++ tests/bridge_parachain_staking_test.py
@@ -37,9 +37,8 @@ class bridge_parachain_staking_test(unittest.TestCase):
         wait_until_block_height(SubstrateInterface(url=RELAYCHAIN_WS_URL), 1)
         wait_until_block_height(SubstrateInterface(url=WS_URL), 1)
 
-    def setUp(self):
-        wait_until_block_height(SubstrateInterface(url=WS_URL), 1)
-
+    def _initialize_connections_and_keypairs(self):
+        """Initialize connections and keypairs"""
         self._substrate = SubstrateInterface(url=WS_URL)
         self._w3 = Web3(Web3.HTTPProvider(ETH_URL))
         self._kp_moon = get_eth_info()
@@ -49,7 +48,22 @@ class bridge_parachain_staking_test(unittest.TestCase):
         self._kp_new_collator = Keypair.create_from_uri('//NewMoon01')
         self._chain_spec = get_modified_chain_spec(get_chain(self._substrate))
 
+    def setUp(self):
+        wait_until_block_height(SubstrateInterface(url=WS_URL), 1)
+        self._initialize_connections_and_keypairs()
+
+    def restart_chain_and_reinit(self):
+        """Restart chain and reinitialize connections"""
+        restart_parachain_and_runtime_upgrade()
+        wait_until_block_height(SubstrateInterface(url=RELAYCHAIN_WS_URL), 1)
+        wait_until_block_height(SubstrateInterface(url=WS_URL), 1)
+        self._initialize_connections_and_keypairs()
+        self._fund_users()
+
+    # Will re generate the moon/mars
     def _fund_users(self, num=100 * 10 ** 18):
+        self._kp_moon = get_eth_info()
+        self._kp_mars = get_eth_info()
         if num < 100 * 10 ** 18:
             num = 100 * 10 ** 18
         # Fund users
@@ -288,6 +302,9 @@ class bridge_parachain_staking_test(unittest.TestCase):
         stake = self.get_stake_number(self._kp_moon['substrate'])
         self.assertEqual(stake['total'], collator_num, f'join fails, stake: {stake}, collator_num: {collator_num}')
 
+        # Verify lock after join_delegators
+        self.verify_delegation_lock(self._kp_moon['substrate'], collator_num, "after join_delegators")
+
         # Delegator stake more
         evm_receipt = self.evm_delegator_stake_more(contract, self._kp_moon['kp'], collator_eth_addr, collator_num)
         self.assertEqual(evm_receipt['status'], 1, f'stake more fails, evm_receipt: {evm_receipt}')
@@ -296,6 +313,9 @@ class bridge_parachain_staking_test(unittest.TestCase):
         stake = self.get_stake_number(self._kp_moon['substrate'])
         self.assertEqual(stake['total'], collator_num * 2, f'join fails, stake: {stake}, collator_num: {collator_num}')
 
+        # Verify lock after delegator_stake_more
+        self.verify_delegation_lock(self._kp_moon['substrate'], collator_num * 2, "after delegator_stake_more")
+
         # Delegator stake less
         evm_receipt = self.evm_delegator_stake_less(contract, self._kp_moon['kp'], collator_eth_addr, collator_num)
         self.assertEqual(evm_receipt['status'], 1, f'stake more fails, evm_receipt: {evm_receipt}')
@@ -303,6 +323,9 @@ class bridge_parachain_staking_test(unittest.TestCase):
         # Check the delegator's stake
         stake = self.get_stake_number(self._kp_moon['substrate'])
         self.assertEqual(stake['total'], collator_num, f'join fails, stake: {stake}, collator_num: {collator_num}')
+
+        # Verify lock after delegator_stake_less
+        self.verify_delegation_lock(self._kp_moon['substrate'], collator_num, "after delegator_stake_less")
 
         # Delegator leave
         evm_receipt = self.evm_delegator_leave_delegators(contract, self._kp_moon['kp'])
@@ -384,6 +407,7 @@ class bridge_parachain_staking_test(unittest.TestCase):
         self.assertEqual(receipt.is_success, True, f'set_commission fails, receipt: {receipt}')
 
     def test_wait_list(self):
+        self.restart_chain_and_reinit()
         contract = get_contract(self._w3, PARACHAIN_STAKING_ADDR, PARACHAIN_STAKING_ABI_FILE)
         out = contract.functions.getCollatorList().call()
         out = sorted(out, key=lambda x: x[1], reverse=True)
@@ -401,6 +425,7 @@ class bridge_parachain_staking_test(unittest.TestCase):
         )
         receipt = batch.execute()
         self.assertEqual(receipt.is_success, True, f'join_collator fails, receipt: {receipt}')
+
         receipt = self.set_commission_rate(10, self._kp_new_collator)
         self.assertEqual(receipt.is_success, True, f'set_commission fails, receipt: {receipt}')
 
@@ -452,6 +477,9 @@ class bridge_parachain_staking_test(unittest.TestCase):
         stake = self.get_stake_number(self._kp_moon['substrate'])
         self.assertEqual(stake['total'], collator_num, f'join fails, stake: {stake}, collator_num: {collator_num}')
 
+        # Verify lock before revoke
+        self.verify_delegation_lock(self._kp_moon['substrate'], collator_num, "before revoke_delegation")
+
         # Delegator leave
         evm_receipt = self.evm_delegator_revoke_delegation(contract, self._kp_moon['kp'], collator_eth_addr)
         self.assertEqual(evm_receipt['status'], 1, f'leave fails, evm_receipt: {evm_receipt}')
@@ -461,6 +489,9 @@ class bridge_parachain_staking_test(unittest.TestCase):
             event['attributes'],
             (self._kp_moon['substrate'], collator_num),
             f'join fails, event: {event}')
+
+        # Verify lock after revoke_delegation (should have 0 delegation but unstaking amount)
+        self.verify_delegation_lock(self._kp_moon['substrate'], 0, "after revoke_delegation")
 
         # Note: The unlock unstaked didn't success because we have to wait about 20+ blocks;
         # therefore, we don't test here. Can just test maunally
@@ -516,6 +547,26 @@ class bridge_parachain_staking_test(unittest.TestCase):
         locks = self._substrate.query('Balances', 'Locks', [account_addr])
         return locks.value
 
+    def verify_delegation_lock(self, account_addr, expected_delegation_total, operation_desc=""):
+        """Verify that lock = delegation.total + unstaking amount"""
+        # Get current locks
+        locks = self.get_balance_locks(account_addr)
+        staking_locks = [lock for lock in locks if lock.get('id') in ['peaqstak', '0x706561717374616b']]
+        self.assertEqual(len(staking_locks), 1, f'Expected exactly one peaqstak lock {operation_desc}. Locks: {locks}')
+
+        # Get unstaking amount from chain
+        unstaking_data = self._substrate.query('ParachainStaking', 'Unstaking', [account_addr])
+        total_unstaking = 0
+        if unstaking_data.value and len(unstaking_data.value) > 0:
+            total_unstaking = unstaking_data.value[0][1]
+
+        # Expected lock = delegation total + unstaking
+        expected_lock = expected_delegation_total + total_unstaking
+        self.assertEqual(staking_locks[0]['amount'], expected_lock,
+                         f'Lock amount mismatch {operation_desc}. '
+                         f'Lock: {staking_locks[0]["amount"]}, Expected: {expected_lock} '
+                         f'(delegation: {expected_delegation_total} + unstaking: {total_unstaking})')
+
     def ensure_two_collators(self, contract):
         """Ensure there are at least 2 collators in the system"""
         out = contract.functions.getCollatorList().call()
@@ -559,7 +610,6 @@ class bridge_parachain_staking_test(unittest.TestCase):
         collator1_eth_addr = out[0][0]
         collator1_stake = out[0][1]
         collator2_eth_addr = out[1][0]
-        collator2_stake = out[1][1]
 
         # Define staking amounts
         high_amount = collator1_stake  # High amount for collator 1
@@ -581,14 +631,10 @@ class bridge_parachain_staking_test(unittest.TestCase):
         # Verify delegator state after first delegation
         delegator_state = self.get_stake_number(self._kp_moon['substrate'])
         self.assertEqual(delegator_state['total'], high_amount,
-                        f'Delegator total stake mismatch after collator1 delegation: {delegator_state}')
+                         f'Delegator total stake mismatch after collator1 delegation: {delegator_state}')
 
-        # Check lock after first delegation
-        locks = self.get_balance_locks(self._kp_moon['substrate'])
-        staking_locks = [lock for lock in locks if lock.get('id') in ['peaqstak', '0x706561717374616b']]
-        self.assertEqual(len(staking_locks), 1, f'Expected exactly one peaqstak lock after first delegation. Locks: {locks}')
-        self.assertEqual(staking_locks[0]['amount'], high_amount,
-                       f'Locked amount ({staking_locks[0]["amount"]}) does not match first delegation amount ({high_amount})')
+        # Verify lock after first delegation
+        self.verify_delegation_lock(self._kp_moon['substrate'], high_amount, "after first delegation (join_delegators)")
 
         # Step 2: Force new round
         receipt = self.force_new_round()
@@ -605,14 +651,10 @@ class bridge_parachain_staking_test(unittest.TestCase):
         delegator_state = self.get_stake_number(self._kp_moon['substrate'])
         expected_total = high_amount + low_amount
         self.assertEqual(delegator_state['total'], expected_total,
-                        f'Delegator total stake mismatch after collator2 delegation: {delegator_state}')
+                         f'Delegator total stake mismatch after collator2 delegation: {delegator_state}')
 
-        # Check lock after second delegation (delegate another candidate)
-        locks = self.get_balance_locks(self._kp_moon['substrate'])
-        staking_locks = [lock for lock in locks if lock.get('id') in ['peaqstak', '0x706561717374616b']]
-        self.assertEqual(len(staking_locks), 1, f'Expected exactly one peaqstak lock after second delegation. Locks: {locks}')
-        self.assertEqual(staking_locks[0]['amount'], expected_total,
-                       f'Locked amount ({staking_locks[0]["amount"]}) does not match total after second delegation ({expected_total})')
+        # Verify lock after second delegation (delegate another candidate)
+        self.verify_delegation_lock(self._kp_moon['substrate'], expected_total, "after delegate_another_candidate")
 
         # Step 4: Force new round again
         receipt = self.force_new_round()
@@ -626,12 +668,15 @@ class bridge_parachain_staking_test(unittest.TestCase):
         delegator_state = self.get_stake_number(self._kp_moon['substrate'])
         final_total = high_amount + low_amount + more_amount
         self.assertEqual(delegator_state['total'], final_total,
-                        f'Delegator total stake mismatch after staking more: {delegator_state}')
+                         f'Delegator total stake mismatch after staking more: {delegator_state}')
+
+        # Verify lock after delegator_stake_more
+        self.verify_delegation_lock(self._kp_moon['substrate'], final_total, "after delegator_stake_more")
 
         # Verify the total on collator 2 is still less than high amount on collator 1
         collator2_total_from_delegator = low_amount + more_amount
         self.assertLess(collator2_total_from_delegator, high_amount,
-                       f'Total on collator2 ({collator2_total_from_delegator}) should be less than high amount on collator1 ({high_amount})')
+                        f'Total on collator2 ({collator2_total_from_delegator}) should be less than high amount on collator1 ({high_amount})')
 
         # Step 6: Check locked tokens equal total staking amount
         locks = self.get_balance_locks(self._kp_moon['substrate'])
@@ -641,4 +686,107 @@ class bridge_parachain_staking_test(unittest.TestCase):
 
         self.assertEqual(len(staking_locks), 1, f'Expected exactly one peaqstak lock, found {len(staking_locks)}. Locks: {locks}')
         self.assertEqual(staking_locks[0]['amount'], final_total,
-                       f'Locked amount ({staking_locks[0]["amount"]}) does not match total staked amount ({final_total})')
+                         f'Locked amount ({staking_locks[0]["amount"]}) does not match total staked amount ({final_total})')
+
+    def test_delegator_multi_collator_unstake_restake_lock_verification(self):
+        """Test lock behavior when delegator unstakes from low collator and then stakes more (less than unstaked amount)"""
+        contract = get_contract(self._w3, PARACHAIN_STAKING_ADDR, PARACHAIN_STAKING_ABI_FILE)
+
+        # Ensure we have at least 2 collators
+        out = self.ensure_two_collators(contract)
+
+        # Get two different collators
+        collator1_eth_addr = out[0][0]
+        collator1_stake = out[0][1]
+        collator2_eth_addr = out[1][0]
+
+        # Define staking amounts
+        high_amount = collator1_stake  # High amount for collator 1
+        low_amount = collator1_stake // 2  # Low amount for collator 2
+        unstake_amount = low_amount // 2  # Unstake half of the low amount from collator 2
+        restake_amount = unstake_amount // 2  # Restake less than unstaked
+
+        # Fund the delegator with enough tokens
+        total_needed = high_amount + low_amount + (10 * 10 ** 18)  # Extra for fees
+        receipt = self._fund_users(total_needed)
+        self.assertEqual(receipt.is_success, True, f'fund_users fails, receipt: {receipt}')
+
+        # Step 1: Delegator stakes high amount on collator 1
+        evm_receipt = self.evm_join_delegators(contract, self._kp_moon['kp'], collator1_eth_addr, high_amount)
+        self.assertEqual(evm_receipt['status'], 1, f'join collator1 fails, evm_receipt: {evm_receipt}')
+
+        # Verify lock after join_delegators
+        self.verify_delegation_lock(self._kp_moon['substrate'], high_amount, "after join_delegators")
+
+        # Step 2: Force new round
+        receipt = self.force_new_round()
+        self.assertEqual(receipt.is_success, True, f'force_new_round fails, receipt: {receipt}')
+
+        # Step 3: Delegator stakes low amount on collator 2
+        evm_receipt = self.evm_delegate_another_candidate(contract, self._kp_moon['kp'], collator2_eth_addr, low_amount)
+        self.assertEqual(evm_receipt['status'], 1, f'delegate another candidate fails, evm_receipt: {evm_receipt}')
+
+        # Verify state after two delegations
+        delegator_state = self.get_stake_number(self._kp_moon['substrate'])
+        expected_total = high_amount + low_amount
+        self.assertEqual(delegator_state['total'], expected_total,
+                         f'Delegator total stake mismatch after two delegations: {delegator_state}')
+
+        # Verify lock after delegate_another_candidate
+        self.verify_delegation_lock(self._kp_moon['substrate'], expected_total, "after delegate_another_candidate")
+
+        # Step 4: Force new round
+        receipt = self.force_new_round()
+        self.assertEqual(receipt.is_success, True, f'force_new_round fails, receipt: {receipt}')
+
+        # Step 5: Unstake from collator 2 (the low staking collator)
+        evm_receipt = self.evm_delegator_stake_less(contract, self._kp_moon['kp'], collator2_eth_addr, unstake_amount)
+        self.assertEqual(evm_receipt['status'], 1, f'stake less from collator2 fails, evm_receipt: {evm_receipt}')
+
+        # Check delegator state after unstaking
+        delegator_state = self.get_stake_number(self._kp_moon['substrate'])
+        expected_total_after_unstake = high_amount + low_amount - unstake_amount
+        self.assertEqual(delegator_state['total'], expected_total_after_unstake,
+                         f'Delegator total stake mismatch after unstake: {delegator_state}')
+
+        # Verify lock after delegator_stake_less (with pending unstake)
+        self.verify_delegation_lock(self._kp_moon['substrate'], expected_total_after_unstake, "after delegator_stake_less")
+
+        # Check that unstaking info exists
+        unstaking_data = self._substrate.query('ParachainStaking', 'Unstaking', [self._kp_moon['substrate']])
+        self.assertTrue(unstaking_data.value, f'No unstaking data found for delegator {self._kp_moon["substrate"]}')
+
+        # Calculate total unstaking amount
+        total_unstaking = sum(entry[1] for entry in unstaking_data.value)
+        self.assertEqual(total_unstaking, unstake_amount,
+                         f'Unstaking amount mismatch: {total_unstaking} != {unstake_amount}')
+
+        # Step 6: Stake more on collator 2 (less than unstaked amount)
+        evm_receipt = self.evm_delegator_stake_more(contract, self._kp_moon['kp'], collator2_eth_addr, restake_amount)
+        self.assertEqual(evm_receipt['status'], 1, f'stake more on collator2 fails, evm_receipt: {evm_receipt}')
+
+        # Verify final delegator state
+        delegator_state = self.get_stake_number(self._kp_moon['substrate'])
+        final_delegator_total = expected_total_after_unstake + restake_amount
+        self.assertEqual(delegator_state['total'], final_delegator_total,
+                         f'Delegator total stake mismatch after restaking: {delegator_state}')
+
+        # Verify lock after final delegator_stake_more (with pending unstake)
+        self.verify_delegation_lock(self._kp_moon['substrate'], final_delegator_total, "after final delegator_stake_more")
+
+        # Step 7: Verify lock = delegator.total + unstaking amount
+        locks = self.get_balance_locks(self._kp_moon['substrate'])
+        staking_locks = [lock for lock in locks if lock.get('id') in ['peaqstak', '0x706561717374616b']]
+        self.assertEqual(len(staking_locks), 1, f'Expected exactly one peaqstak lock. Locks: {locks}')
+
+        # Get current unstaking amount from chain
+        unstaking_data = self._substrate.query('ParachainStaking', 'Unstaking', [self._kp_moon['substrate']])
+        current_total_unstaking = 0
+        if unstaking_data.value and len(unstaking_data.value) > 0:
+            current_total_unstaking = unstaking_data.value[0][1]
+
+        # The lock should be delegator total + current pending unstake amount
+        expected_lock = final_delegator_total + current_total_unstaking
+        self.assertEqual(staking_locks[0]['amount'], expected_lock,
+                         f'Lock amount ({staking_locks[0]["amount"]}) does not match expected '
+                         f'(delegator.total: {final_delegator_total} + unstaking: {current_total_unstaking} = {expected_lock})')
