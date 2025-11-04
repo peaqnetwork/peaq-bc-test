e306c0d61abed05d158bacd399d1b5b9cdb54e82
jaypan <pan@eotlabs.io>
2025-11-04 11:28:11 +0100

    Add generous timeout to wait_for_n_blocks calls using DEFAULT_BLOCK_TIME
    Use 117 * DEFAULT_BLOCK_TIME as timeout parameter for wait_for_n_blocks calls
    in monkey_submit_extrinsic and _wait_finalization.
    
    For 6 sec blocks: 117 * 6 = 702 seconds (~11.7 min)
    For 12 sec blocks: 117 * 12 = 1404 seconds (~23.4 min)
    
    This prevents timeout errors during runtime upgrades when blocks take longer
    to produce, especially when Docker container struggles with new runtime.

--- tools/monkey/monkey_3rd_substrate_interface.py
+++ tools/monkey/monkey_3rd_substrate_interface.py
@@ -8,6 +8,7 @@ import socket
 
 
 from peaq.utils import wait_for_n_blocks
+from tools.constants import DEFAULT_BLOCK_TIME
 
 
 def _wait_finalization(substrate, included_block):
@@ -16,7 +17,7 @@ def _wait_finalization(substrate, included_block):
         print(f'Checking finalized block {finalized_block} and included block {included_block}')
         if finalized_block >= included_block:
             break
-        wait_for_n_blocks(substrate, 1)
+        wait_for_n_blocks(substrate, 1, 117 * DEFAULT_BLOCK_TIME)
 
 
 def monkey_patch():
@@ -49,7 +50,7 @@ def monkey_submit_extrinsic(self, extrinsic: GenericExtrinsic, wait_for_inclusio
     if not wait_for_inclusion and not wait_for_finalization:
         return result
 
-    wait_for_n_blocks(self, 4)
+    wait_for_n_blocks(self, 4, 117 * DEFAULT_BLOCK_TIME)
     now_block_num = self.get_block_number(None)
     included_block = None
     tx_identifier = None
